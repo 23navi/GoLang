78d6c05a66a9fba60b2cbb1e177e0271bcc79840
23navi <navisureka23@gmail.com>
2024-10-12 11:59:05 +0530

    write to file

--- SG/cards/main.go
+++ SG/cards/main.go
@@ -1,8 +1,15 @@
 package main
 
+import "io/ioutil"
+
 func main() {
-	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+	// const abc int = 20
+	// fmt.Print(abc)
+
+	ioutil.WriteFile("test.txt", []byte("Hello World"), 0666)
+
 }
