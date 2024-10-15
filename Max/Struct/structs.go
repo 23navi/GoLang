8e984763cb05497395510905f8fc58adc06a8c6d
23navi <navisureka23@gmail.com>
2024-10-15 22:37:03 +0530

    pointers basics

--- Max/Struct/structs.go
+++ Max/Struct/structs.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 )
 
+func printSomething(name *string) {
+	fmt.Println(*name)
+}
+
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	printSomething(&firstName)
 
 	// ... do something awesome with that gathered data!
 
